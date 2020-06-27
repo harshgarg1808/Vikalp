9763dbb233fcda57dd24d30e1267d31006411329
harshgarg1808 <63161821+harshgarg1808@users.noreply.github.com>
2020-06-27 12:09:02 +0530

    Add files via upload

--- Code Cosmos/Form/app.js
+++ Code Cosmos/Form/app.js
@@ -1,29 +1,63 @@
-const fields = document.querySelectorAll('input');
-const send = document.querySelector('.form-group');
 
-send.addEventListener('click', onClick);
-function onClick(e) {
-  e.preventDefault();
+    function validateName() {
+            var name = document.getElementById('name').value;
+            if(name.length == 0) {
+              alert("Name can't be blank") ;
+              return false;
 
-  fields.forEach((x) => {
-    if(x.value === '') {
-      console.log(x.value);
-      x.parentElement.classList.add('error-tag');
-    }else {
-      x.parentElement.classList.remove('error-tag');
-      if(x.type == 'email'){
-        if (validateEmail(x.value)) {
-          x.parentElement.classList.remove('error-tag');
-        }else {
-          x.parentElement.classList.add('error-tag');
+            }
+            if (!name.match(/^[a-zA-Z]{3,}(?: [a-zA-Z]+){0,2}$/)) {
+              alert("Please enter your correct name") ;//Validation Message
+              return false;
+            }
+            return true;
+          }
+
+          function validatePhone() {
+            var phone = document.getElementById('phone').value;
+            if(phone.length == 0) {
+              alert("Phone number can't be blank") ;//Validation Message
+              return false;
+            }
+
+            if(!phone.match(/^[0]?[789]\d{9}$/)) {
+             alert("Please enter a correct phone number") ;//Validation Message
+             return false;
+           }
+
+           return true;
+
+         }
+
+         function validateEmail () {
+
+          var email = document.getElementById('email').value;
+          if(email.length == 0) {
+            alert("Email can't be blank") ;//Validation Message
+            return false;
+
+          }
+
+          if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+            alert("Please enter a correct email address") ;//Validation Message
+            return false;
+
+          }
+
+          return true;
+
         }
-      }
-    }
-  })
-}
 
-function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-} 
 
+        function validateForm() {
+          if (!validateName() || !validatePhone() || !validateEmail()) {
+
+            alert("Form not submitted");//Validation Message
+            return false;
+          }
+          else {
+            submitted=true;
+            return true;
+          }
+        }
+        
